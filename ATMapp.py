298061ba2bdba2e87a5b837af79586dde2990195
ramon20panes <ragoex20@gmail.com>
2025-04-15 14:34:23 +0200

    Últimas pruebas 4

--- ATMapp.py
+++ ATMapp.py
@@ -133,7 +133,7 @@ else:
     if "redirect_to" in st.session_state and st.session_state.redirect_to:
         target_page = st.session_state.redirect_to
         st.session_state.redirect_to = None  # Limpiar
-        st.switch_page(f"pages/{target_page}")
+        st.switch_page(target_page)
 
 # Espacio antes del footer
 st.markdown('<div style="margin-top: 30px;"></div>', unsafe_allow_html=True)
